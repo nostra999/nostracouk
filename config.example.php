c6c2ee930d078227329ec0d3429b8b05bb776ae1
Richard Moseley <richard@nostra.co.uk>
2014-03-23 11:45:52 +0000

    update to config file

--- config.example.php
+++ config.example.php
@@ -1,5 +1,10 @@
 <?php
+//1. update mysql credentials as follows
+
 $db_un = 'username';
 $db_pw = 'password';
 $db_db = 'database';
+
+//2. run following SQL on server to create required tables
+
 ?>
